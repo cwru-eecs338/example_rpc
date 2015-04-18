e2525040a0f63202dc3698bde2de3ec467ccd5ea
CarlinJ216 <carlinj216@live.com>
2015-04-18 05:22:40 -0400

    Delete unnecessary scripts
    Signed-off-by: CarlinJ216 <carlinj216@live.com>

--- compile_client.sh
+++
@@ -1,4 +0,0 @@
-gcc -c hello_clnt.c
-gcc -c hello_client.c
-gcc -o client hello_client.o hello_clnt.o
-
--- compile_server.sh
+++
@@ -1,4 +0,0 @@
-gcc -c hello_svc.c
-gcc -c hello_server.c
-gcc -o server hello_server.o hello_svc.o  
-
